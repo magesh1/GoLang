416f35db3c9ee275210c1735b0635831cd647e64
magesh <48727934+magesh1@users.noreply.github.com>
2022-01-21 13:35:40 +0530

    Add files via upload

---
+++ basic/Calculator.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 10
+	b := 20
+	var c int
+	c = a + b
+
+	fmt.Println("addition:",c)
+
+	c = a-b
+	fmt.Println("subtraction:",c)
+
+	c = a*b
+	fmt.Println("multiplication:",c)
+
+	c = a/b
+	fmt.Println("division:",c)
+
+
+
+
+
+}
---
+++ basic/go.mod
@@ -0,0 +1,3 @@
+module github.com/magesh1/GoLang/basic
+
+go 1.17
