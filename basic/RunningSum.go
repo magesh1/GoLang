141f8c406844a5fa6d566fe05d4082ce8813b9d5
magesh <48727934+magesh1@users.noreply.github.com>
2022-01-28 17:10:12 +0530

    Update RunningSum.go

--- basic/RunningSum.go
+++ basic/RunningSum.go
@@ -8,13 +8,13 @@ func main() {
   findSum(nums)
 }
 
-func findSum(nums []int) []int {
+func findSum(nums []int) {
 
 for i := 1;i < len(nums);i++ {
         nums[i] = nums[i-1]+nums[i]
 }
     
     
-    return nums;
+  fmt.Println(nums)
   
 }
