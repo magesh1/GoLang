a5f63b8ddb3bc768b24bc746c02754acd6be3cbb
magesh <48727934+magesh1@users.noreply.github.com>
2022-01-28 17:06:33 +0530

    Create RunningSum.go

---
+++ basic/RunningSum.go
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/running-sum-of-1d-array/
+
+package main
+import "fmt"
+
+func main() {
+  nums := []int{1,2,3,4} 
+  findSum(nums)
+}
+
+func findSum(nums []int) []int {
+
+for i := 1;i < len(nums);i++ {
+        nums[i] = nums[i-1]+nums[i]
+}
+    
+    
+    return nums;
+  
+}
