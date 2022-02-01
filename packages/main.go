9f1b219e12d7a6a89c44d9bdaef5970e22cc6d6c
magesh <48727934+magesh1@users.noreply.github.com>
2022-02-01 16:30:12 +0530

    Add files via upload

---
+++ packages/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	// "fmt"
+
+	"github.com/magesh1/GoLang/packages/oddeven"
+	"github.com/magesh1/GoLang/packages/printer"
+	
+)
+
+func main() {
+
+	num := 1
+	flag := math.Evenodd(num,false)
+	prints.PrintList(num,flag)
+
+	
+
+
+
+
+}
